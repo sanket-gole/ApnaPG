0bf6b6e3958c2e310549ef727976dce7d0914cd8
Rohit Gurav <rohitgurav0602@gmail.com>
2025-04-12 10:48:00 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
-
+//test
 const listingSchema = new Schema({
     title: {
         type: String,
